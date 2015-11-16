83906379434b45fcb4438088b502a789974fbb02
awais <rajaawais31@gmail.com>
2015-11-16 14:34:14 +0500

    First Project 2

---
+++ calculater/src/pk/ciit/calculater/Demo.java
@@ -0,0 +1,12 @@
+package pk.ciit.calculater;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hy .here is my first project of GITHUB");
+
+	}
+
+}
